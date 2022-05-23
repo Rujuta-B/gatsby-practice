62e65c49a4badbe60b2783f5eb304d394a96e605
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-05-23 18:10:16 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql, Link } from 'gatsby'
+import Layout from "../components/Layout"
+import styles from '../styles/home.module.css'
+
+export default function Home({ data }) {
+  console.log(data)
+  const { title, description } = data.site.siteMetadata
+
+  return (
+    <Layout>
+      <section >
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+        </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        <p>{ title } - { description }</p>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
