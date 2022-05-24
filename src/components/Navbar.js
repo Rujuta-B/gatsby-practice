615eb4a9500f0fcd01d17e69ba5da671cae38f42
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-05-24 12:40:03 +0530

    component query

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 export default function Navbar() {
+  const data = useStaticQuery(graphql`
+    query MyQuery{
+      site{
+        siteMetadata{
+          title
+        }
+      }
+    }
+  `)
+
+  const { title } = data.site.siteMetadata
+
   return (
     <nav>
       <Link to="/">
-        {" "}
-        <h1>Web Warrior</h1>
+       <h1>{title}</h1>
       </Link>
       <div className="links">
         <Link to="/about">About</Link>
